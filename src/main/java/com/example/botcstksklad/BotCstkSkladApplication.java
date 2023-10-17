02cdfffebd8240ea2e5552ea64fd22caf367e621
Pivovarov Maksim <120288@mail.ru>
2023-10-17 20:09:36 +0600

    первый коммит

---
+++ src/main/java/com/example/botcstksklad/BotCstkSkladApplication.java
@@ -0,0 +1,13 @@
+package com.example.botcstksklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotCstkSkladApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotCstkSkladApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/botcstksklad/BotCstkSkladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botcstksklad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCstkSkladApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
