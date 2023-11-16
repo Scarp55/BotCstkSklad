c62f2b90d3a1c5da24d031f84e6b87995b64ebdb
Pivovarov Maksim <120288@mail.ru>
2023-11-17 01:09:15 +0600

    update git ignore

--- src/main/java/com/example/botcstksklad/model/Bot.java
+++ src/main/java/com/example/botcstksklad/model/Bot.java
@@ -3,13 +3,22 @@ package com.example.botcstksklad.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@PropertySource("classpath:bot.properties")
 public class Bot {
+
+    @Value("${bot.name}")
     String name;
+
+    @Value("${bot.token}")
     String token;
+
     final String uriGetUpdates = "https://api.telegram.org/bot" + token + "/getUpdates";
+
     final String uriSendMessage = "https://api.telegram.org/bot" + token + "/sendMessage";
 }
